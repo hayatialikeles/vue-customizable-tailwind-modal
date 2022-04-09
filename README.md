c33f97fd71cecde073d898d3bf06c21a686533ce
hayatialikeles <38311526+hayatialikeles@users.noreply.github.com>
2022-04-09 13:22:27 +0300

    init

--- README.md
+++ README.md
@@ -1,29 +1,59 @@
-# vue-tailwind-modal
+# VUE JS CUSTOMİZABLE TAILWIND MODAL
 
-## Project setup
-```
-npm install
-```
+customizable tailwind modal is a vue.js library.
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
+## Installation
 
-### Compiles and minifies for production
-```
-npm run build
+Use the package manager npm to install .
+
+```bash
+npm i vue-customizable-tailwind-modal
 ```
 
-### Run your unit tests
-```
-npm run test:unit
-```
+## Usage
 
-### Lints and fixes files
-```
-npm run lint
-```
+```javascript
+import modal from vue-customizable-tailwind-modal
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+componentes:{
+   modal
+},
+
+  <modal 
+        @closed="this event is modal closed  trigger"
+        @opened="this event is modal opened trigger"
+        toggleBtnClass="w-full rounded p-2"
+        containerClass="w-6/12  shadow h-80 bg-gray-200 relative rounded"
+        title="Modal title"
+        headerContainerClass="flex justify-between card-header p-4 border-b border-opacity-50 border-black"
+        closeBtnClass="w-6 h-6 text-black"
+        bodyContainerClass="p-4"
+        footerClass=""
+    >
+
+        <template v-slot:open-btn-slot>
+            Open Modal
+        </template> 
+        <template v-slot:header>
+            <div :class="headerContainerClass">
+                <h5>{{ title }}</h5>
+                <button @click="closeModal(true)" :class="closeBtnClass">X</button>
+            </div>
+        </template>
+    
+        modal content detail
+
+        <template v-slot:footer>
+            Open Modal
+        </template>
+    </modal>
+
+
+```
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+Please make sure to update tests as appropriate.
+
+## License
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,97 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button :class="toggleBtnClass" @click="() => openModal()">
+      <slot name="open-btn-slot"> Open Modal </slot>
+    </button>
+
+    <Transition name="fade">
+      <div
+        class="modal-overlay h-screen flex items-center justify-center w-full fixed top-0 bottom-0 left-0 right-0 bg-black bg-opacity-30"
+        style="z-index: 99 !important"
+        v-if="show"
+        @click="closeModal()"
+      >
+        <div
+          :class="containerClass"
+          @mouseover="() => (isModalOver = true)"
+          @mouseleave="() => (isModalOver = false)"
+          style="z-index: 999 !important"
+        >
+          <slot name="header">
+            <div :class="headerContainerClass">
+              <h5>{{ title }}</h5>
+              <button @click="closeModal(true)" :class="closeBtnClass">X</button>
+            </div>
+          </slot>
+          <div :class="bodyContainerClass">
+            <slot>body content </slot>
+          </div>
+
+          <footer :class="footerClass">
+            <slot name="footer"> </slot>
+          </footer>
+        </div>
+      </div>
+    </Transition>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  props: {
+    toggleBtnClass: {
+      default: "w-full rounded p-2",
+    },
+    containerClass: {
+      default: "w-6/12  shadow h-80 bg-gray-200 relative rounded",
+    },
+    title: {
+      default: "Modal Title",
+    },
+    headerContainerClass: {
+      default:
+        "flex justify-between card-header p-4 border-b border-opacity-50 border-black",
+    },
+    closeBtnClass: {
+      default: "w-6 h-6 text-black",
+    },
+    bodyContainerClass: {
+      default: "p-4",
+    },
+    footerClass: {
+      default: "",
+    },
+  },
+  data() {
+    return {
+      show: false,
+      isModalOver: false,
+    };
+  },
+  methods: {
+    openModal() {
+      this.show = true;
+      this.$emit("opened", true);
+    },
+    closeModal(closeBtn) {
+      if (!this.isModalOver || closeBtn) {
+        this.show = false;
+        this.$emit("closed", true);
+      }
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.2s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
